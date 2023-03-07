ce2bc746582d45212e410632ec7a442bd530b499
AshleyK18-marcey <akastler18@gmail.com>
2023-03-07 21:33:00 +0000

    updated driver

--- Driver.h
+++ Driver.h
@@ -1,10 +1,24 @@
 #ifndef DRIVER_H
 #define DRIVER_H
 
+
 class Driver {
 public:
-    Driver();
-private:
-};
+	
+	static Driver& GetInstance() {
+		// gets instantiated the first time
+		static Driver instance; // guaranteed to be destroyed
 
-#endif  // DRIVER_H header guard
+		return instance;
+	}
+
+	// Delete the methods we don't want
+	Driver(Driver const&) = delete; // copy constructor
+	void operator=(Driver const&) = delete; // assignment operator
+ private:
+ 	Driver();  // private constructor
+
+  
+};  // class Driver
+
+#endif  // Driver_H
--- Users.h
+++ Users.h
@@ -10,26 +10,34 @@ public:
 
   std::string get_address();  
 
-  void UpdateInformation();
+  void SetName(std::string newName);
 
+  void SetPhone(std::string newPhone);
 
-   
+  void SetAddress(std::string newAddress);
+
+  void UpdateInformation(std::string infoToChange, std::string newValue);
+
 private:
+
   float balance_;
   unsigned int userid_;
   std::string name_;
   std::string phone_;
   std::string address_;
+
 };
 class Seller: public User{
 public:
-  Product* addProductForSale(Product object); 
+  void addProductForSale(Product* object); 
+
+  void AddBalanace(float amount);
 
   void OpenBid(Product* product);
 
   void CloseBid(Product* product); 
 
-  void sendMessage();
+ 
   
 private:
   std::vector<Products*> open_bids_;
@@ -39,9 +47,12 @@ private:
 };
 class Buyer: public User{
 public:
+
+  void DeductBalanace(float amount);
+  
   void addBidToProduct();
 
   void sendMessage();
 private:
-  // add more fields as applicable
+  
 };
